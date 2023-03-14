d8c244015dadc270c02a19b21727318de8276ee3
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-03-14 23:56:37 +0100

    improved item/mob-clear

--- plugins/basic/itemClear.js
+++ plugins/basic/itemClear.js
@@ -5,14 +5,17 @@ module.exports = function inject(bot, options) {
     bot.chatAddPattern(/^\[GrieferGames\] Es wurden (\d+) auf dem Boden liegende Items entfernt!$/, 'itemClear')
 
     bot.itemClear = {
+        nextAt: null,
         events: new EventEmitter()
     }
 
     bot.on('itemClearIn', (secs) => {
+        bot.itemClear.nextAt = Date.now() + (secs * 1000)
         bot.itemClear.events.emit('itemClearIn:' + secs)
     })
 
     bot.on('itemClear', (removedItems) => {
+        bot.itemClear.nextAt = Date.now() + (20 * 60 * 1000)
         bot.itemClear.events.emit('itemClear', removedItems)
     })
 }
--- plugins/basic/mobRemover.js
+++ plugins/basic/mobRemover.js
@@ -5,14 +5,17 @@ module.exports = function inject(bot, options) {
     bot.chatAddPattern(/^\[MobRemover\] Es wurden (\d+) Tiere entfernt\.$/, 'mobRemover')
     
     bot.mobRemover = {
+        nextAt: null,
         events: new EventEmitter()
     }
 
     bot.on('mobRemoverIn', (mins) => {
+        bot.mobRemover.nextAt = Date.now() + (+mins * 60 * 1000)
         bot.mobRemover.events.emit('mobRemoverIn:' + mins)
     })
 
     bot.on('mobRemover', (removedMobs) => {
+        bot.mobRemover.nextAt = Date.now() + (20 * 60 * 1000)
         bot.mobRemover.events.emit('mobRemover', removedMobs)
     })
 }
