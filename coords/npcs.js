5952a039f36843319dad250dd06d8caf579a751c
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-03-15 11:38:45 +0100

    refactored npcs.js

--- coords/npcs.js
+++ coords/npcs.js
@@ -1,59 +1,72 @@
 module.exports = {
     "stadt": {
-        "Händler": { // Orb-Händler
+        "Orb-Händler": {
+            "npcIdentifier": "Händler",
             "openedWindowTitleRegex": /^Händler$/,
             "position": [172, 25, -42]
         },
-        "Verkäufer": { // Orb-Verkäufer
+        "Orb-Verkäufer": {
+            "npcIdentifier": "Verkäufer",
             "openedWindowTitleRegex": /^Verkäufer$/,
             "position": [172, 25, -31]
         },
-        "Statistik": { // Orb-Statistik
+        "Orb-Statistik": {
+            "npcIdentifier": "Statistik",
             "openedWindowTitleRegex": /^Statistik von (\S+)$/,
             "position": [140, 25, -39]
         },
 
         "Adventurer": {
-            "openedWindowTitleRegex": /^Verkäufer$/,
+            "npcIdentifier": "Adventurer",
+            "openedWindowTitleRegex": /^Adventure-Jobs$/,
             "position": [145, 24, 3]
         },
-        "Admin-Shop": { // Adventurer Admin-Shop
-            "openedWindowTitleRegex": /^Verkäufer$/,
+        "Adventurer Admin-Shop": {
+            "npcIdentifier": "Admin-Shop",
+            "openedWindowTitleRegex": /^Shop$/,
             "position": [147, 24, 1]
         },
 
-        "Vote-System": {
+        "VoteTyp": {
+            "npcIdentifier": "Vote-System",
             "openedWindowTitleRegex": /^Vote-System$/,
             "position": [129, 25, -18]
         },
         "Lotterie": {
+            "npcIdentifier": "Lotterie",
             "openedWindowTitleRegex": /^Lotterie$/,
             "position": [139, 25, -30]
         },
         "Rand-Schmied": {
+            "npcIdentifier": "Rand-Schmied",
             "openedWindowTitleRegex": /^Rand-Konfigurator$/,
             "position": [140, 25, -54]
         },
-        
+
     },
     "hub": {
-        "Skyblock Museum": {
+        "Skyblock": {
+            "npcIdentifier": "Skyblock Museum",
             "action": "teleport",
             "position": [42, 68, -182]
         },
-        "Citybuild 1.19": {
+        "CB1.19": {
+            "npcIdentifier": "Citybuild 1.19",
             "action": "teleport",
             "position": [20, 71, -170]
         },
-        "Citybuild 1.8": {
+        "CB1.8": {
+            "npcIdentifier": "Citybuild 1.8",
             "action": "teleport",
             "position": [16, 71, -170]
         },
         "Impressum": {
+            "npcIdentifier": "Impressum",
             "action": "chat",
             "position": [6, 71, -167]
         },
         "Datenschutz": {
+            "npcIdentifier": "Datenschutz",
             "action": "chat",
             "position": [2, 71, -171]
         }
