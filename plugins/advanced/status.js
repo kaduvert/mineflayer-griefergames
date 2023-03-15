2099e599b44edfe15b082559f5fad3f4161632fe
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-03-15 09:42:51 +0100

    added status

--- plugins/advanced/status.js
+++ plugins/advanced/status.js
@@ -1,5 +1,19 @@
-[Status] Du hast 'text asdasdasd' als Status gesetzt.
-[Status] Status-Befehle 
-[Status] Du hast den Status beim Joinen aktiviert.
-[Status] Du hast den Status beim Joinen deaktiviert.
-[Status] Du hast keinen Supreme-Rang. Besuche unseren Shop unter shop.griefergames.net!
+// const EventEmitter = require('events')
+
+module.exports = function inject(bot, options) {
+	bot.chatAddPattern(/^\[Status\] Du hast '(.+)' als Status gesetzt\.$/, 'statusMessageSet')
+	bot.chatAddPattern(/^\[Status\] Du hast den Status beim Joinen aktiviert\.$/, 'statusActivated')
+	bot.chatAddPattern(/^\[Status\] Du hast den Status beim Joinen deaktiviert\.$/, 'statusDeactivated')
+
+	bot.chatAddPattern(/^\[Status\] Du hast keinen (.+)-Rang\. Besuche unseren Shop unter (.+)!$/, 'statusInsufficientPermissionsError')
+
+	bot.status = {}
+
+    bot.status.set = (message) => {
+        return bot.chat.getChatActionResult(`/status ${message}`, 'statusMessageSet', ['statusInsufficientPermissionsError'], 5000)
+    }
+
+    bot.status.toggle = () => {
+        return bot.chat.getChatActionResult('/status toggle', ['statusActivated', 'statusDeactivated'], ['statusInsufficientPermissionsError'], 5000)
+    }
+}
