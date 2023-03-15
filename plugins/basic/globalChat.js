a34d964bc3265066c21e988a2d2b6b56f5642aae
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-03-15 08:16:59 +0100

    added globalChat

--- plugins/basic/globalChat.js
+++ plugins/basic/globalChat.js
@@ -1,12 +1,43 @@
+const EventEmitter = require('events')
+
 module.exports = function inject(bot, options) {
     bot.chatAddPattern(/^@\[(\S+)\] (\S+) ┃ (\S+) » (.*)$/, 'globalChatMessage')
+    bot.chatAddPattern(/^\[GlobalChat\] Du kannst nicht im Global-Chat schreiben, da du ihn deaktiviert hast\.$/, 'globalChatDeactivatedError')
 
-    // send method
+    bot.chatAddPattern(/^\[GlobalChat\] Du hast den Global-Chat aktiviert\.$/, 'globalChatActivated')
+    bot.chatAddPattern(/^\[GlobalChat\] Du hast den Global-Chat deaktiviert\.$/, 'globalChatDeativated')
+
+    bot.chatAddPattern(/^\[GlobalChat\] Der Global-Chat ist bereits aktiviert\.$/, 'globalChatAlreadyActivated')
+    bot.chatAddPattern(/^\[GlobalChat\] Der Global-Chat ist bereits deaktiviert\.$/, 'globalChatAlreadyDeativated')
+
+    bot.chatAddPattern(/^\[GlobalChat\] Du hast den Server (.+) gemutet\.$/, 'globalChatServerMuted')
+    bot.chatAddPattern(/^\[GlobalChat\] Du hast den Server (.+) entmutet\.$/, 'globalChatServerMuted')
+
+    bot.globalChat = {
+        events: new EventEmitter()
+    }
+
+    bot.globalChat.login = () => {
+        return bot.chat.getChatActionResult('/globalchat login', 'globalChatActivated', ['globalChatAlreadyActivated'], 5000)
+    }
+
+    bot.globalChat.logout = () => {
+        return bot.chat.getChatActionResult('/globalchat logout', 'globalChatDeativated', ['globalChatAlreadyDeativated'], 5000)
+    }
+
+    bot.globalChat.send = (message) => {
+        return bot.chat.getChatActionResult(`@${message}`, 'globalChatSentMessage', ['globalChatDeactivatedError'], 5000, bot.globalChat.events)
+    }
+
+    bot.on('globalChatMessage', (cb, rank, username, message) => {
+        if (username === bot.username) {
+            bot.globalChat.events.emit('globalChatSentMessage', cb, rank, username, message)
+        }
+    })
 }
 
-[GlobalChat] Der Global-Chat ist bereits deaktiviert.
-[GlobalChat] Du hast den Global-Chat aktiviert.
-[GlobalChat] Der Global-Chat ist bereits aktiviert.
-[GlobalChat] Du hast den Global-Chat deaktiviert.
-[GlobalChat] Du hast den Server Evil entmutet.
-[GlobalChat] Du hast den Server Evil gemutet.
+
+
+
+
+
