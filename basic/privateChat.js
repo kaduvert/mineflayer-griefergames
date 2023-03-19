3b02a394d4c026341dd503d7fc4f44fb226be310
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-03-19 18:20:51 +0100

    updated privateChat

--- basic/privateChat.js
+++ basic/privateChat.js
@@ -5,28 +5,38 @@ module.exports = function inject(bot, options) {
 	bot.privateChat = {}
 
 	bot.privateChat.send = (username, msg) => {
-		const prefix = privateChat.commands.send(username)
-		const charLimit = 100 - prefix.length
-		while (msg.length > charLimit) {
-			const splitIndex = msg.substring(0, charLimit).lastIndexOf(' ') + 1 || charLimit
-			bot.chat.send(prefix + msg.substring(0, splitIndex))
-			bot.chat.getChatActionResult(prefix + msg.substring(0, splitIndex), 'chat:privateChatSentMessage', ['playerNotFoundError'], 5000)
-			msg = msg.substring(splitIndex)
-		}
-		bot.chat.getChatActionResult(prefix + msg, 'chat:privateChatSentMessage', ['playerNotFoundError'], 5000)
+		return bot.chat.getChatActionResult(
+			bot.buildCommand(privateChat.commands.send, username, msg),
+			'chat:privateChatSentMessage',
+			['chat:receiverToggledMessagesError', 'chat:playerNotFoundError'],
+			5000
+		)
+	}
+
+	bot.privateChat.toggle = () => {
+		return bot.chat.getChatActionResult(
+			privateChat.commands.toggle,
+			['chat:privateChatActivated', 'chat:privateChatDeactivated'],
+			[],
+			5000
+		)
+	}
+
+	bot.privateChat.toggleIgnore = (username) => {
+		return bot.chat.getChatActionResult(
+			bot.buildCommand(privateChat.commands.ignore, username),
+			['chat:ignoreAdd', 'chat:ignoreRemove'],
+			[],
+			5000
+		)
+	}
+
+	bot.privateChat.getIgnoredList = () => {
+		return bot.chat.getChatActionResult(
+			privateChat.commands.ignoredList,
+			['chat:ignoreList', 'chat:ignoreListEmpty'],
+			[],
+			5000
+		)
 	}
 }
-
-/*
-// msgtoggle
-Das Empfangen von Nachrichten wurde deaktiviert.
-Das Empfangen von Nachrichten wurde aktiviert.
-Ultra ┃ ColaKanone_HD hat Nachrichten deaktiviert.
-
-// ignore
-
-Du ignorierst ab jetzt Spieler Airasoon.
-Du ignorierst Spieler Airasoon nicht mehr.
-
-Ignoriert: !spionelefant557 1enton
-*/
