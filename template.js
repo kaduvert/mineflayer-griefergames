d5252cf96d35a7ef6324cd14581eea72d717812e
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-03-19 21:13:37 +0100

    updated template

--- template.js
+++ template.js
@@ -2,12 +2,23 @@
 // const EventEmitter = require('events')
 
 module.exports = function inject(bot, options) {
-	bot.chatAddPattern(/^$/, '')
+	const data = bot.ggData.data
+	bot.chat.loadPatterns(data)
+	// bot.window.loadPatterns(data)
 
 	bot. = {}
 
-	return bot.chat.getChatActionResult(''``, '', [], 5000)
 
+	return bot.chat.getChatActionResult(''``, '', [], 5000)
+	return bot.window.getClickActionResult(
+		window,
+		bot.window.getMatchingItem(window, data.itemPatterns.purchaseToken).slot,
+		0,
+		'windowOpen:',
+		[],
+		1000
+	)
+	
 }
 
 */
