c7213ab6c621b584f2adb2e6c440d656175cc27e
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-03-23 10:04:34 +0100

    removed mysteryMod spoof, added using data from custom mysterymod channel

--- basic/mysteryMod.js
+++ basic/mysteryMod.js
@@ -1,18 +1,43 @@
-// const EventEmitter = require('events')
+const EventEmitter = require('events')
 
 module.exports = function inject(bot, options) {
     const mysteryMod = bot.loadPatternsAndGetData('mysteryMod')
 
     bot.mysteryMod = {
-        // events: new EventEmitter()
+        events: new EventEmitter()
+    }
+
+    bot.mysteryMod.events.on('redstone', (data) => {
+        if (+data.status === 0) return
+        console.log(`DEBUG: event mysteryMod.events->redstone triggered with unusual data:\n${data}`)
+    })
+
+    bot.mysteryMod.events.on('bank', (data) => {
+        bot.bank.balance = +data.amount
+    })
+
+    bot.mysteryMod.events.on('countdown_create', (data) => {
+        if (data.name === 'ClearLag') {
+            if (data.unit === 'SECONDS') {
+                bot.itemClear.nextAt = Date.now() + (data.until * 1000)
+            }
+        } else {
+            console.log(`DEBUG: event mysteryMod.events->countdown_create triggered with unusual data:\n${data}`)
+        }
+    })
+
+    bot.mysteryMod.sendPayload = (jsonData) => {
+        bot._client.write('custom_payload', { channel: mysteryMod.packetChannelName, data: Buffer.from(JSON.stringify(jsonData)) })
     }
 
     bot._client.on('packet', (data, meta) => {
         if (meta.name !== 'custom_payload' || data.channel !== mysteryMod.packetChannelName) return
-        const check = Buffer.from(data.data).toString()
-        // console.log(check)
-        if (check.includes('mysterymod_user_check')) {
-            bot._client.write('custom_payload', { channel: mysteryMod.packetChannelName, data: Buffer.from(check.substring(check.indexOf('@'))) })
-        } // TODO: handle other payloads
+        const buf = data.data
+        const bufString = buf.toString()
+        const eventEndIndex = buf[0] + 1
+        const eventName = bufString.substring(1, eventEndIndex)
+        const eventData = bufString.substring(eventEndIndex + 1, bufString.length)
+
+        bot.mysteryMod.events.emit(eventName, JSON.parse(eventData))
     })
 }
