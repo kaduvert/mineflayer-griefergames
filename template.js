f806bee6e8e4ef1ad119a93cc21a312f136af5ad
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-03-31 19:20:17 +0200

    refactored to use new object argument

--- template.js
+++ template.js
@@ -2,23 +2,13 @@
 // const EventEmitter = require('events')
 
 module.exports = function inject(bot, options) {
-	const pluginId = bot.loadPatternsAndGetData('data')
+	const data = bot.loadPatternsAndGetData('data')
 	
 	// 
 
-	bot. = {}
+	bot.data = {}
 
 
-	return bot.chat.sendFallibe(''``, '', [], 5000)
-	return bot.window.click(
-		window,
-		bot.window.getMatchingItem('', '', window).slot,
-		0,
-		'windowOpen:',
-		[],
-		1000
-	)
-	
 }
 
 */
