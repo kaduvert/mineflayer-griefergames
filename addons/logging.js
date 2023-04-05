0335ba55bc17feeedefce5ba068753127ddf3dfb
KadaverBrutalo10 <jimmybiel123@gmail.com>
2023-04-05 11:38:34 +0200

    added logging addon

---
+++ addons/logging.js
@@ -0,0 +1,14 @@
+module.exports = function inject(bot, options) {
+    const chalk = require('chalk')
+    bot.chat.log = (msg, pos) => {
+		if (bot.noLog) return
+		const str = msg.toString()
+		if (pos === 'game_info' || str === '»' || str === '') return
+		console.log(chalk.cyan(bot.timeStamp()), msg.toAnsi())
+	}
+
+	bot.on('kicked', function logKick(reason) {
+		reason = JSON.parse(reason).text
+		console.log(chalk.red(bot.timeStamp()), chalk.yellowBright(`Kicked: ${chalk.red(reason)}`))
+	})
+}
