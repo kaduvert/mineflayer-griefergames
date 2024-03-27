927477c1963ba4124cfaefc6eb7bdbab912a248f
KadaverBrutalo10 <jimmybiel123@gmail.com>
2024-03-27 21:06:57 +0100

    move addons to extra repo

--- addons/antiAfk.js
+++
@@ -1,3 +0,0 @@
-module.exports = function load(bot, ns) {
-    bot.on('windowOpen:afk->challange', window => window.solveChallange())
-}
--- addons/mysteryModSpoof.js
+++
@@ -1,3 +0,0 @@
-module.exports = function load(bot, ns) {
-    bot.on('misc:mysteryMod->mysterymod_user_check', bot.mysteryMod.sendPayload)
-}
