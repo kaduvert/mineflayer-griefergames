1a9db530fd08695b8cd2eee96b692e8dc394c5fd
kaduvert <jimmybiel123@gmail.com>
2024-06-17 18:19:55 +0200

    make nextAt date object

--- src/basic/itemClear.js
+++ src/basic/itemClear.js
@@ -4,12 +4,12 @@ module.exports = function load(bot, ns) {
     ns.itemClear.nextAt = null
 
     bot.on('chat:itemClear->nextIn', ([[secs]]) => {
-        ns.itemClear.nextAt = Date.now() + (secs * 1000)
+        ns.itemClear.nextAt = new Date(Date.now() + (secs * 1000))
         bot.emit('misc:itemClear->in:' + secs)
     })
 
     bot.on('chat:itemClear->removedItems', ([[removedItems]]) => {
-        ns.itemClear.nextAt = Date.now() + (20 * 60 * 1000)
+        ns.itemClear.nextAt = new Date(Date.now() + (20 * 60 * 1000))
         bot.emit('misc:itemClear', removedItems)
     })
 }
--- src/basic/mobRemover.js
+++ src/basic/mobRemover.js
@@ -4,12 +4,12 @@ module.exports = function load(bot, ns) {
     ns.mobRemover.nextAt = null
 
     bot.on('chat:mobRemover->nextIn', ([[mins]]) => {
-        ns.mobRemover.nextAt = Date.now() + (+mins * 60 * 1000)
+        ns.mobRemover.nextAt = new Date(Date.now() + (+mins * 60 * 1000))
         bot.emit('misc:mobRemover->in:' + mins)
     })
 
     bot.on('chat:mobRemover->removedMobs', ([[removedMobs]]) => {
-        ns.mobRemover.nextAt = Date.now() + (mobRemover.cycleTime)
+        ns.mobRemover.nextAt = new Date(Date.now() + (mobRemover.cycleTime))
         bot.emit('misc:mobRemover', removedMobs)
     })
 }
