957e218e0c39dc32cbc1393c9864d27a535e7c7f
Michi <freimichael3@gmail.com>
2024-11-30 15:48:04 +0100

    fix ending errors

--- src/modules/coinflip/data.js
+++ src/modules/coinflip/data.js
@@ -1,12 +1,12 @@
 
 module.exports = {
     chatPatterns: {
-        win: /^Herzlichen Glückwunsch! Du hast die Summe von (\d+)\$ auf (\d+)\$ verdoppelt!\.$/,
-        loss: /^\Du hast leider verloren und (\d+)\$ verloren.\.$/,
+        win: /^Herzlichen Glückwunsch! Du hast die Summe von (\d+)\$ auf (\d+)\$ verdoppelt!$/,
+        loss: /^\Du hast leider verloren und (\d+)\$ verloren\.$/,
         help: /^Verwendung \/coinflip <Betrag>\.$/,
 
-        invalidNumberError: /^Gib eine Zahl als Betrag an.\.$/,
-        insufficientAmountError: /^Du hast nicht genug Geld.\.$/
+        invalidNumberError: /^Gib eine Zahl als Betrag an\.$/,
+        insufficientAmountError: /^Du hast nicht genug Geld\.$/
     },
     commands: {
         play: '/coinflip $1'
