615546a38c0582e86eb9c3d150539ce9825bd376
kaduvert <jimmybiel123@gmail.com>
2024-12-02 19:20:36 +0100

    make loss success case & rename

--- src/modules/coinflip/data.js
+++ src/modules/coinflip/data.js
@@ -1,20 +1,20 @@
 module.exports = {
     chatPatterns: {
-        win: /^\[Coinflip\] Herzlichen Glückwunsch! Du hast die Summe von ([\d\.,]+)\$ auf ([\d\.,]+)\$ verdoppelt!$/,
-        loss: /^\[Coinflip\] Du hast leider verloren und ([\d\.,]+)\$ verloren\.$/,
+        flipWon: /^\[Coinflip\] Herzlichen Glückwunsch! Du hast die Summe von ([\d\.,]+)\$ auf ([\d\.,]+)\$ verdoppelt!$/,
+        flipLost: /^\[Coinflip\] Du hast leider verloren und ([\d\.,]+)\$ verloren\.$/,
         help: /^\[Coinflip\] Verwendung \/coinflip <Betrag>$/,
 
         deactivatedError: /^\[Coinflip\] Coinflip ist deaktiviert\.$/,
         invalidNumberError: /^\[Coinflip\] Gib eine Zahl als Betrag an\.$/,
-        insufficientAmountError: /^\[Coinflip\] Du hast nicht genug Geld\.$/
+        insufficientBalanceError: /^\[Coinflip\] Du hast nicht genug Geld\.$/
     },
     commands: {
         play: '/coinflip $1'
     },
     chatActions: {
         play: {
-            successEvent: 'win',
-            failureEvents: ['deactivatedError', 'invalidNumberError', 'insufficientAmountError', 'loss']
+            successEvents: ['flipWon', 'flipLost'],
+            failureEvents: ['deactivatedError', 'invalidNumberError', 'insufficientBalanceError']
         }
     }
 }
